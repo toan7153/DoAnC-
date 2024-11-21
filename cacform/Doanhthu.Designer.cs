6cea4d64218694c213722aae4449ff9d11d79806
toan7153 <voduytoan6a@gmail.com>
2024-11-21 13:37:56 +0700

    Initial commit

---
+++ cacform/Doanhthu.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class Doanhthu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpcheckIn = new System.Windows.Forms.DateTimePicker();
+            this.dtpcheckOut = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnViewBill = new System.Windows.Forms.Button();
+            this.dgvBill = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBill)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(411, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(396, 39);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "QUẢN LÝ DOANH THU";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(64, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chọn thời gian muốn xem doanh thu:";
+            // 
+            // dtpcheckIn
+            // 
+            this.dtpcheckIn.Location = new System.Drawing.Point(68, 123);
+            this.dtpcheckIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpcheckIn.Name = "dtpcheckIn";
+            this.dtpcheckIn.Size = new System.Drawing.Size(339, 22);
+            this.dtpcheckIn.TabIndex = 6;
+            // 
+            // dtpcheckOut
+            // 
+            this.dtpcheckOut.Location = new System.Drawing.Point(507, 124);
+            this.dtpcheckOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpcheckOut.Name = "dtpcheckOut";
+            this.dtpcheckOut.Size = new System.Drawing.Size(307, 22);
+            this.dtpcheckOut.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 130);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Đến";
+            // 
+            // btnViewBill
+            // 
+            this.btnViewBill.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnViewBill.FlatAppearance.BorderSize = 0;
+            this.btnViewBill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewBill.ForeColor = System.Drawing.Color.White;
+            this.btnViewBill.Location = new System.Drawing.Point(872, 112);
+            this.btnViewBill.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnViewBill.Name = "btnViewBill";
+            this.btnViewBill.Size = new System.Drawing.Size(167, 49);
+            this.btnViewBill.TabIndex = 9;
+            this.btnViewBill.Text = "Thống kê";
+            this.btnViewBill.UseVisualStyleBackColor = false;
+            this.btnViewBill.Click += new System.EventHandler(this.btnViewBill_Click);
+            // 
+            // dgvBill
+            // 
+            this.dgvBill.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBill.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBill.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvBill.Location = new System.Drawing.Point(0, 190);
+            this.dgvBill.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvBill.Name = "dgvBill";
+            this.dgvBill.RowHeadersWidth = 51;
+            this.dgvBill.Size = new System.Drawing.Size(1224, 540);
+            this.dgvBill.TabIndex = 10;
+            // 
+            // Doanhthu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 730);
+            this.Controls.Add(this.dgvBill);
+            this.Controls.Add(this.btnViewBill);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpcheckOut);
+            this.Controls.Add(this.dtpcheckIn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Doanhthu";
+            this.Text = "Doanhthu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBill)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpcheckIn;
+        private System.Windows.Forms.DateTimePicker dtpcheckOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnViewBill;
+        private System.Windows.Forms.DataGridView dgvBill;
+    }
+}
